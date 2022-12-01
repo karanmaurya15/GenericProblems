1042a1ea1d02b23e58436ae5c9b01ed770f1d4ee
karanmaurya15 <karanmaurya9663@gmail.com>
2022-12-01 17:19:07 +0530

    Test case for 2nd position  is done

--- GenericProblems/Program.cs
+++ GenericProblems/Program.cs
@@ -5,7 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Wellcome to Generics Program\n");
-            int output = MaxValue.MaxIntegerNumber(222, 3333, 11);
+            int output = MaxValue.MaxIntegerNumber(11, 3333, 222);
             Console.WriteLine("Max integer number is: " + output);
         }
     }
