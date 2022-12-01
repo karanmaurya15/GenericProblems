99c944fc04f91627b7d7bee7f2548ec14f187c44
karanmaurya15 <karanmaurya9663@gmail.com>
2022-12-01 17:40:31 +0530

    Test case for 2nd position  is done

--- GenericProblems/Program.cs
+++ GenericProblems/Program.cs
@@ -5,7 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Wellcome to Generics Program\n");
-            float output = MaxValue.MaxFloatNumber(3.333f, 2.22f, 1.1f);
+            float output = MaxValue.MaxFloatNumber(2.22f, 3.333f, 1.1f);
             Console.WriteLine("Max Float number is: " + output);
         }
     }
