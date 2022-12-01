432e1c988956c37c9dc139ff4dba67a7d7e5ffc9
karanmaurya15 <karanmaurya9663@gmail.com>
2022-12-01 17:31:40 +0530

    Max Float is done

--- GenericProblems/MaxValue.cs
+++ GenericProblems/MaxValue.cs
@@ -1,14 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace GenericProblems
 {
     internal class MaxValue
     {
-        public static int MaxIntegerNumber(int value1, int value2, int value3)
+        public static float MaxFloatNumber(float value1, float value2, float value3)
         {
             if (value1.CompareTo(value2) > 0 && value1.CompareTo(value3) > 0)
             {
@@ -22,8 +17,8 @@ namespace GenericProblems
             {
                 return value3;
             }
-            return value3;
-           // throw new Exception("firstValue, secondValue and thirdValue are same");
+            //return value1;
+            throw new Exception("firstValue, secondValue and thirdValue are same");
 
         }
     }
--- GenericProblems/Program.cs
+++ GenericProblems/Program.cs
@@ -5,7 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Wellcome to Generics Program\n");
-            int output = MaxValue.MaxIntegerNumber(11, 222, 3333);
+            float output = MaxValue.MaxFloatNumber(1.1f, 2.22f, 3.333f);
             Console.WriteLine("Max integer number is: " + output);
         }
     }
