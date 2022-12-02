0da79c643cbc4309fd0ffac946461c3c94af6812
karanmaurya15 <karanmaurya9663@gmail.com>
2022-12-02 17:15:38 +0530

    Extend Max Method taking 4 Parameter

--- GenericProblems/MaxValue.cs
+++ GenericProblems/MaxValue.cs
@@ -1,35 +1,55 @@
-﻿
-namespace GenericProblems
+﻿namespace GenericProblems
 {
     internal class MaxValue<T> where T : IComparable
     {
-        public T Value1, Value2, Value3;
-        public MaxValue(T value1, T value2, T value3)
+        public T value1, value2, value3, value4;
+
+        public MaxValue(T value1, T value2, T value3, T value4)
         {
-            this.Value1 = value1;
-            this.Value2 = value2;
-            this.Value3 = value3;
+            this.value1 = value1;
+            this.value2 = value2;
+            this.value3 = value3;
+            this.value4 = value4;
         }
-        public static T MaxValueCheck(T value1, T value2, T value3)
-        { 
-            if (value1.CompareTo(value2) > 0 && value1.CompareTo(value3) > 0)
+
+        public static T MaxNumber(T value1, T value2, T value3, T value4)
+        {
+            if (value1.CompareTo(value2) > 0 && value1.CompareTo(value3) > 0 && value1.CompareTo(value4) > 0)
             {
                 return value1;
             }
-            if (value2.CompareTo(value1) > 0 && value2.CompareTo(value3) > 0)
+            if (value2.CompareTo(value1) > 0 && value2.CompareTo(value3) > 0 && value2.CompareTo(value4) > 0)
             {
                 return value2;
             }
-            if (value3.CompareTo(value1) > 0 && value3.CompareTo(value2) > 0)
+            if (value3.CompareTo(value1) > 0 && value3.CompareTo(value2) > 0 && value3.CompareTo(value4) > 0)
             {
                 return value3;
             }
+            if (value4.CompareTo(value1) > 0 && value4.CompareTo(value2) > 0 && value4.CompareTo(value3) > 0)
+            {
+                return value4;
+            }
             return default;
         }
+
         public T MaxMethod()
         {
-            T max = MaxValue<T>.MaxValueCheck(this.Value1, this.Value2, this.Value3);
+            T max = MaxValue<T>.MaxNumber(this.value1, this.value2, this.value3, this.value4);
             return max;
         }
+        // Using Sorting
+        public T[] values;
+        public MaxValue(T[] values)
+        {
+            this.values = values;
+            
+        }
+        public T[] Sort(T[] values)
+        {
+            Array.Sort(values);
+            return values;
+        }
+       
     }
 }
--- GenericProblems/Program.cs
+++ GenericProblems/Program.cs
@@ -9,17 +9,33 @@ namespace GenericProblems
         {
             Console.WriteLine("Wellcome to Generics Program");
 
-            MaxValue<int> maxValueint = new MaxValue<int>(11, 222, 3333);
+            MaxValue<int> maxValueint = new MaxValue<int>(11, 222, 3333, 4444);
             int output1 = maxValueint.MaxMethod();
-            Console.WriteLine("\nMax integer number is: " + output1);
+            Console.WriteLine("\nMax integer number is : " + output1 );
 
-            MaxValue<float> maxValuefloat = new MaxValue<float>(1.1f, 2.22f, 3.333f);
-            float output2 = maxValuefloat.MaxMethod();
-            Console.WriteLine("\nMax float number is: " + output2);
+            MaxValue<float> maxValueFloat = new MaxValue<float>(1.1f, 22.2f, 3.333f, 44.44f);
+            float output2 = maxValueFloat.MaxMethod();
+            Console.WriteLine("\nMax integer number is : " + output2);
 
-            MaxValue<string> maxValuestring = new MaxValue<string>("Apple", "Peach", "Banana");
-            string output3 = maxValuestring.MaxMethod();
-            Console.WriteLine("\nMax String is: " + output3);
+            MaxValue<string> maxValueString = new MaxValue<string>("Apple", "Peach", "Banana", "Pineapple");
+            string output3 = maxValueString.MaxMethod();
+            Console.WriteLine("\nMax integer number is : " + output3);
+
+            // Using Sorting
+            Console.WriteLine("\n~~~~~Using Sorting~~~~~");
+            int[] array = { 14, 564, 733, 448 };
+            MaxValue<int> maxValueInt = new MaxValue<int>(array);
+            var SortedArray = maxValueInt.Sort(array);
+            Console.Write("\nSoeted values are : ");
+            foreach (int item in SortedArray)
+            {
+              
+                Console.Write( " " + item);
+            }
+            Console.WriteLine();
+            var max = SortedArray[SortedArray.Length-1];
+            Console.WriteLine("\nMax value is " + max);
+          
         }
     }
 }
